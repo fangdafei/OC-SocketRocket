c8537707cc8892f54487427b93a6e04c2f360fab
fangrifei <fangrifei@100tal.com>
2018-07-11 22:51:18 +0800

    测试xcode--github

--- TestWebSocket/AppDelegate.m
+++ TestWebSocket/AppDelegate.m
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    //修改了
+    
     return YES;
 }
 
