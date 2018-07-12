73435eac7b4bc294089ba6add08ecf972d295f0a
fangrifei <fangrifei@100tal.com>
2018-07-12 15:03:41 +0800

    add new

--- TestWebSocket/AppDelegate.m
+++ TestWebSocket/AppDelegate.m
@@ -18,8 +18,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    //修改了
-    
     return YES;
 }
 
